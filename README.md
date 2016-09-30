0bf68e35a30c1bce4be957dbea32730eecbfcbe3
lion <lion@MacBookPro.local>
2016-09-30 11:59:30 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # alipay_python_sdk
-alipay_python_sdk
+
+## 说明
+
+关于支付宝接口的完全使用方式,请参考阿里官方文档 [文档目录](https://doc.open.alipay.com/) [API目录](https://doc.open.alipay.com/doc2/apiList?docType=4)
+
+本项目仅抽象调用过程,方便py开发者与alipay对接
+
+## 安装依赖
+
+```bash
+git clone https://github.com/lioncui/alipay_python_sdk
+cd alipay_python_sdk
+pip install pycrypto
+```
+
+## 请求示例
+
+```py
+Python 2.7.10 (default, Oct 23 2015, 19:19:21) 
+[GCC 4.2.1 Compatible Apple LLVM 7.0.0 (clang-700.0.59.5)] on darwin
+Type "help", "copyright", "credits" or "license" for more information
+>>> from alipay_python_sdk import AliPayClient
+>>> ali = AliPayClient()
+>>> ali.gateway = "https://openapi.alipay.com/gateway.do"
+>>> ali.app_id = "2016081612345678"
+>>> ali.private_key_path = "/secret/private.pem"
+>>> ali.method = "monitor.heartbeat.syn"
+>>> ali.biz_content = "{}"
+>>> ali.request()
+{u'monitor_heartbeat_syn_response': {u'msg': u'Success', u'code': u'10000', u'pid': u'2016081612345678'}, u'sign': u'ndNbTnj62ZSW3YV2TkMher96IUP37kYrZvEsiqd1ce8I6iAH8GLgiYAyojJ6+xXrWk3enTOvWRPRBOIqwS+TZEiKPdDbllz9BZMn2KkZSHy7XllzlBw0LfSyfTmO/O5qZycNMC4a5ZkLF4gaaBasyrM4SRskg4eaPzzFiC5EDvM='}
+```
---
+++ __init__.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import base64
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5 as PK
+from Crypto.Hash import SHA
+import json
+from datetime import datetime
+try:
+    from urllib import urlencode
+except:
+    from urllib.parse import urlencode
+try:
+    import urllib2
+except:
+    from urllib import request as urllib2
+
+
+class AliPayClient(object):
+
+    def __init__(self):
+        self.gateway = "https://openapi.alipay.com/gateway.do"
+        self.app_id = ""
+        self.format = "JSON"
+        self.method = ""
+        self.charset = "utf-8"
+        self.sign_type = "RSA"
+        self.timestamp = ""
+        self.version = "1.0"
+        self.notify_url = ""
+        self.app_auth_token = ""
+        self.biz_content = ""
+
+    def _sys_params(self):
+        data = {
+            "app_id": self.app_id,
+            "format": self.format,
+            "method": self.method,
+            "charset": self.charset,
+            "sign_type": self.sign_type,
+            "timestamp": get_timestamp(),
+            "version": self.version,
+            "app_auth_token": self.app_auth_token,
+            "notify_url": self.notify_url,
+        }
+        self.sys_params = data
+        return self.sys_params
+
+    def _api_params(self):
+        data = {
+            "biz_content": self.biz_content,
+        }
+        self.api_params = data
+        return self.api_params
+
+    def _check_empty(self, key, **params):
+        return params.get(key) != "" and not None
+
+    def _get_sign_content(self, **params):
+        redata = []
+        sorted_params = sorted(params)
+        for k in sorted_params:
+            if self._check_empty(k, **params):
+                element = "{0}={1}".format(k, params[k])
+                redata.append(element)
+        sign_content = "&".join(redata)
+        return sign_content
+
+    def _get_sign_with_sha1(self, private_key, content):
+        key = RSA.importKey(private_key)
+        h = SHA.new(content.encode("utf-8"))
+        signer = PK.new(key)
+        sign = signer.sign(h)
+        return base64.b64encode(sign).decode("utf-8")
+
+    def request(self):
+        totalParams = self._sys_params().copy()
+        totalParams.update(self._api_params())
+        signcontent = self._get_sign_content(**totalParams)
+        with open(self.private_key_path) as f:
+            private_key = f.read()
+        sign = self._get_sign_with_sha1(private_key, signcontent)
+        self.sys_params["sign"] = sign
+        querys = urlencode(self.sys_params)
+        post_data = urlencode(self.api_params)
+        url = "{0}?{1}".format(self.gateway, querys)
+        response = urllib2.urlopen(url, post_data).read().decode("utf-8")
+        return json.loads(response)
+
+    def verify(self, **params):
+        with open(self.alipay_public_key_path) as f:
+            alipay_public_key = f.read()
+        pub_key = RSA.importKey(alipay_public_key)
+        verifier = PK.new(pub_key)
+        sign = params.pop('sign')
+        if 'sign_type' in params:
+            params.pop('sign_type')
+        charset = params.get("charset", 'utf-8')
+        content = self.get_sign_content(**params)
+        data = SHA.new(content.encode(charset))
+        return verifier.verify(data, base64.b64decode(sign))
+
+
+def get_timestamp():
+    return datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
